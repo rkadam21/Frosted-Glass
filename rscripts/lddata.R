4ba7668a864ad73030f4b5020c86923e144b922f
rkadam21 <coderk21@gmail.com>
2015-10-01 10:14:20 -0400

    Load data from Excel
    Added commentary and certain tweaks to the function

--- rscripts/lddata.R
+++ rscripts/lddata.R
@@ -1,8 +1,20 @@
+#####Function for read data from Excel#######
+# Title :- Read data from Excel
+# Author:- Rahul Kadam
+# Date :- 09/27/2015
+# Version:- 1.1
+# Description:- The function is used to read data from excel files and load in into data frames.
+#               The function allows to use a mapping document to map the data elements to internal standard
+#               mappings. It also allows to name the data frames in the mapping document.
+###########################################
+## idn = list of sheets that need to be read
+## datafileloc = location for data file
+## mapfileloc = location for mapping file
 
 lddata <-function(idn,datafileloc,mapfileloc)
 {
-  dfmp <- read.xlsx(mapfileloc,sheetIndex=1)
-  for (i in seq_along(idn))
+  dfmp <- read.xlsx(mapfileloc,sheetIndex=1) ###reading the mapping document
+  for (i in seq_along(idn)) ### cycle thru multiple sheets if needed
   {  
     temp <<- read.xlsx(datafileloc, sheetName=idn[i],n=1)
     colnames(temp) <<- as.character(dfmp$NewFieldName[dfmp$SheetName==idn[i]])
